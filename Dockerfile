7ae4d1dec8b956edc59f1ae54dd05c341c0a011b
Koichi Murakami <koichi.murakami@kek.jp>
2023-02-19 03:15:34 +0900

    set version to 8.7

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 #
 # Dockerfile for AlmaLinux instance
 #
-FROM almalinux:latest
+FROM almalinux:8.7
 LABEL maintener="Koichi Murakami <koichi.murakami@kek.jp>"
 
 #
--- build_image.sh
+++ build_image.sh
@@ -7,7 +7,7 @@ export DOCKER_BUILDKIT=1
 
 echo "@@ Build Docker image ..."
 
-target=koichimurakamik6/almalinux:latest
+target=koichimurakamik6/almalinux:8.7
 
 id=`docker image ls -q $target`
 
